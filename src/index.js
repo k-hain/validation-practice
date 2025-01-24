7cb43209d7e333780194da5c0fb42054dbda42ea
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-01-25 00:09:31 +0100

    Add postal code validation
    Refactored FormInput class for easy extension.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ import './style.css';
 const formEl = document.querySelector('form');
 const formInputs = [];
 
-class FormInput {
+
+class FormInput {   
     constructor(nameStr, nameClass) {
         this.name = nameStr;
         this.inputEl = document.getElementById(`${nameClass}`);
         this.errorEl = document.querySelector(`.${nameClass} .error`);
+
+        this.constraintList = [
+            {
+                name: 'valueMissing',
+                message: `This field cannot be empty.`,
+            },
+            {
+                name: 'typeMismatch',
+                message: `Entered value needs to be a valid ${this.name}.`,
+            }
+        ];
 
         this.inputEl.addEventListener('input', () => {
             this.validateInput();
@@ -17,11 +29,12 @@ class FormInput {
     }
 
     showError() {
-        if (this.inputEl.validity.valueMissing) {
-            this.errorEl.textContent = `This field cannot be empty.`;
-        } else if (this.inputEl.validity.typeMismatch) {
-            this.errorEl.textContent = `Entered value needs to be a valid ${this.name}.`;
-        }
+        this.constraintList.forEach((constraint) => {
+            if (this.inputEl.validity[`${constraint.name}`]) {
+                this.errorEl.textContent = constraint.message;
+            }
+        });
+
         this.errorEl.classList.add('active');
     }
 
@@ -35,9 +48,22 @@ class FormInput {
     }
 }
 
+class FormInputPostalCode extends FormInput {
+    constructor(nameStr, nameClass) {
+        super(nameStr, nameClass);
+        this.inputEl.setAttribute('pattern', '[0-9]{2}-[0-9]{3}');
+        this.constraintList.push(
+            {
+                name: 'patternMismatch',
+                message: `Entered value needs to be a valid ${this.name} containing exactly 5 digits (e.g. 12-345).`,
+            }
+        );
+    }
+}
+
 const email = new FormInput('email address', 'email');
 const country = new FormInput('country', 'country');
-const postalCode = new FormInput('postal code', 'postal-code');
+const postalCode = new FormInputPostalCode('postal code', 'postal-code');
 const password = new FormInput('password', 'password');
 formInputs.push(email, country, postalCode, password);
 
