317067925c396f2f29c312a65dbadb920768d0da
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-01-25 01:13:44 +0100

    Add country name validiation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import './style.css';
 import PubSub from 'pubsub-js';
+import countries from 'country-json/src/country-by-name.json';
 
 /* global document */
 
 const formEl = document.querySelector('form');
 const formInputs = [];
+
 const PASSWORD_UPDATE = 'PASSWORD UPDATE';
+
+let countryNames = '';
+countries.forEach((entry) => {
+    let updatedNames = countryNames.concat('|', entry.country);
+    countryNames = updatedNames;
+})
 
 class FormInput {   
     constructor(nameStr, nameClass) {
@@ -49,6 +57,19 @@ class FormInput {
     }
 }
 
+class FormInputCountry extends FormInput {
+    constructor(nameStr, nameClass) {
+        super(nameStr, nameClass);
+        this.inputEl.setAttribute('pattern', countryNames);
+        this.constraintList.push(
+            {
+                name: 'patternMismatch',
+                message: `Entered value needs to be a valid country name.`,
+            }
+        );
+    }
+}
+
 class FormInputPostalCode extends FormInput {
     constructor(nameStr, nameClass) {
         super(nameStr, nameClass);
@@ -56,7 +77,7 @@ class FormInputPostalCode extends FormInput {
         this.constraintList.push(
             {
                 name: 'patternMismatch',
-                message: `Entered value needs to be a valid ${this.name} containing exactly 5 digits (e.g. 12-345).`,
+                message: `Entered value needs to be a valid postal code containing exactly 5 digits (e.g. 12-345).`,
             }
         );
     }
@@ -90,7 +111,7 @@ class FormInputPasswordConfirmation extends FormInput {
 }
 
 const emailInput = new FormInput('email address', 'email');
-const countryInput = new FormInput('country', 'country');
+const countryInput = new FormInputCountry('country', 'country');
 const postalCodeInput = new FormInputPostalCode('postal code', 'postal-code');
 const passwordInput = new FormInputPassword('password', 'password');
 const passwordConfirmationInput = new FormInputPasswordConfirmation('password confirmation', 'password-confirmation');
